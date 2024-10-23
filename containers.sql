9310f41acfa2014b2b074bd6e57dc880895921eb
khuynh134 <khuynh134@csu.fullerton.edu>
2024-10-23 12:09:52 -0700

    added to containers.sql

--- containers.sql
+++ containers.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE containers;
 
+CREATE TABLE containers (containernumber varchar, nameofship varchar, containersize float, datecontainershipped varchar);
+INSERT INTO containers VALUES('123','COSCO Star',50,'2024-01-01');
+INSERT INTO containers VALUES('456J','MAERSK Rock',25.600000000000000532,'2024-03-08');
+INSERT INTO containers VALUES('x1','Betty',34,'2024-05-14');
+CREATE UNIQUE INDEX idx_containers_containerNumber ON containers (containerNumber);
